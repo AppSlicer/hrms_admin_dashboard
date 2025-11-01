1d10a065ab5644177103741e483f9cdf8c2b00f7
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-02 00:04:30 +0600

    Layout added

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -1,3 +1,20 @@
+import type {ReactNode} from "react";
+import Navbar from "@/components/section/Navbar.tsx";
+import Sidebar from "@/components/section/Sidebar.tsx";
+// import {useSelector} from "react-redux";
+// import type {RootState} from "@/redux/stores/store.ts";
 
+export default function DashboardLayout ({ children }:{ children: ReactNode }) {
 
-export default () => {}
+    // const { user, token } = useSelector( (state: RootState) => state.auth )
+
+    return (
+        <main className={"w-full h-[88vh]"}>
+            <Navbar />
+            <div className={"w-full h-full flex"}>
+                <Sidebar />
+                <div className={"w-full h-full bg-gray-100"}>{ children }</div>
+            </div>
+        </main>
+    )
+}
