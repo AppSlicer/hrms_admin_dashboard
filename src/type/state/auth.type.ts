b2d1c2fc8fc56ddc888e7d541dd9fbd1d62b0ef9
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-01 20:52:39 +0600

    Auth type

--- src/type/state/auth.type.ts
+++ src/type/state/auth.type.ts
@@ -1,5 +1,6 @@
+import type {USER_ROLE} from "@/type/user/role.type.ts";
 
 export interface AuthState {
-    user: null | { id: string; name: string };
+    user: null | { id: string; name: string; role: USER_ROLE };
     token: string | null;
 }
