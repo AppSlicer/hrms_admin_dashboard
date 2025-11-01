6d2fe8f17899af9cd844b943e7abb930a5642628
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-01 22:36:41 +0600

    Added the global state on the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import Router from "./router/Router.tsx";
-import {Provider} from "react-redux";
-import {store} from "./redux/stores/store.ts";
 import {Toaster} from "sonner";
+import {useEffect} from "react";
+import Cookies from "js-cookie";
+import {TOKEN_NAME} from "@/enum/token.enum.ts";
+import {useDispatch} from "react-redux";
+import {login} from "@/redux/reducers/authSlice.ts";
 
 export default function App () {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        const token = Cookies.get(TOKEN_NAME.HRM_FIRM);
+        const user = Cookies.get(TOKEN_NAME.USER_INFO);
+
+        if ( user && token )
+            dispatch(login({token: JSON.parse(token), user: JSON.parse(user)}));
+        else if ( !user && !token )
+            dispatch(login({token: undefined, user: undefined}));
+
+    }, []);
+
     return (
-        <Provider store={store}>
+        <>
             <Router />
             <Toaster />
-        </Provider>
+        </>
     )
 }
