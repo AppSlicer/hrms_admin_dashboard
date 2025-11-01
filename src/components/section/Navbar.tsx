983425717043c193d2318855edc889ba5c087ee9
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-01 23:53:52 +0600

    Navbar created

---
+++ src/components/section/Navbar.tsx
@@ -0,0 +1,51 @@
+import ImageWithSkeleton from "@/components/ui/ImageWIthSkeleton.tsx";
+import Logo from "/icons/logo.png";
+
+// Static imports
+import userIcon from "/icons/users.png";
+import messagesIcon from "/icons/messages.png";
+import notificationIcon from "/icons/notification.png";
+import Profile from "../ui/Profile.tsx";
+import {useNavigate} from "react-router-dom";
+
+export default function Navbar () {
+
+    const navigate = useNavigate();
+    const user = 2;
+    const notification = 8;
+    const messages = 9;
+
+    return (
+        <nav className={"w-full h-[106px] border-b border-black"}>
+            <div className={"w-full h-full px-6 mx-auto flex justify-between items-center"}>
+                <div className={"w-[248px] h-[64px]"}>
+                     <ImageWithSkeleton src={Logo} />
+                </div>
+                <div className={"min-w-[120px] h-full flex justify-between items-center gap-[20px]"}>
+                    <div
+                        onClick={() => navigate("")}
+                        className={"relative cursor-pointer flex justify-center items-center w-[40px] h-[40px] bg-red-300"}
+                    >
+                        <ImageWithSkeleton src={userIcon} />
+                        <span className={"absolute bottom-0 w-[15px] h-[15px] text-white flex justify-center items-center text-xs rounded-full bg-red-700"}>{user}</span>
+                    </div>
+                    <div
+                        onClick={() => navigate("")}
+                        className={"relative cursor-pointer flex justify-center items-center w-[40px] h-[40px] bg-red-300"}
+                    >
+                        <ImageWithSkeleton src={messagesIcon} />
+                        <span className={"absolute bottom-0 w-[15px] h-[15px] text-white flex justify-center items-center text-xs rounded-full bg-red-700"}>{messages}</span>
+                    </div>
+                    <div
+                        onClick={() => navigate("")}
+                        className={"relative cursor-pointer flex justify-center items-center w-[40px] h-[40px] bg-red-300"}
+                    >
+                        <ImageWithSkeleton src={notificationIcon} />
+                        <span className={"absolute bottom-0 w-[15px] h-[15px] text-white flex justify-center items-center text-xs rounded-full bg-red-700"}>{notification}</span>
+                    </div>
+                    <Profile />
+                </div>
+            </div>
+        </nav>
+    )
+}
