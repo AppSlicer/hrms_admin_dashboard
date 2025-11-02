11ddae7c1f1b611720326e426fb231ff6db9ab67
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-02 22:00:57 +0600

    tab enum added

---
+++ src/enum/tab.enum.ts
@@ -0,0 +1,103 @@
+import {
+    Banknote,
+    Calendar1,
+    CalendarDays,
+    CalendarMinus, ChartNetwork, ClipboardPlus,
+    CreditCard,
+    LucideLayoutDashboard, MessageCircleQuestionMark, ReceiptText, Siren, TrainFrontTunnel, UserRound,
+    UsersRound
+} from "lucide-react";
+
+export const admin = [
+    {
+        icon: LucideLayoutDashboard,
+        title: "Overview",
+    },
+    {
+        icon: TrainFrontTunnel,
+        title: "Sub Admin Management",
+    },
+    {
+        icon: UserRound,
+        title: "Employee Management",
+    },
+    {
+        icon: ChartNetwork,
+        title: "Profit",
+    },
+    {
+        icon: ClipboardPlus,
+        title: "HRMC Audit Report",
+    },
+    {
+        icon: UsersRound,
+        title: "Audit Log",
+    },
+    {
+        icon: CreditCard,
+        title: "Subscription Management",
+    },
+    {
+        icon: ReceiptText,
+        title: "Terms & Condition",
+    },
+    {
+        icon: Siren,
+        title: "Privacy Policy",
+    },
+    {
+        icon: UserRound,
+        title: "My Profile",
+    },
+    {
+        icon: MessageCircleQuestionMark,
+        title: "Help & Support",
+    },
+] as const;
+
+export const subAdmin = [
+    {
+        icon: LucideLayoutDashboard,
+        title: "Overview",
+    },
+    {
+        icon: UsersRound,
+        title: "User Management",
+    },
+    {
+        icon: Calendar1,
+        title: "Attendance Management",
+    },
+    {
+        icon: CalendarMinus,
+        title: "Leave Management",
+    },
+    {
+        icon: CalendarDays,
+        title: "Task Management",
+    },
+    {
+        icon: Banknote,
+        title: "Pay Sli",
+    },
+    {
+        icon: CreditCard,
+        title: "Subscription Management",
+    },
+    {
+        icon: ReceiptText,
+        title: "Terms & Condition",
+    },
+    {
+        icon: Siren,
+        title: "Privacy Policy",
+    },
+    {
+        icon: UserRound,
+        title: "My Profile",
+    },
+    {
+        icon: MessageCircleQuestionMark,
+        title: "Help & Support",
+    },
+] as const;
