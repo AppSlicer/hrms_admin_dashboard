39825794b0387954c9073caf4d4429c64f2fbca2
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-02 23:54:56 +0600

    Comment removed

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -1,19 +1,14 @@
 import type {ReactNode} from "react";
 import Navbar from "@/components/section/Navbar.tsx";
 import Sidebar from "@/components/section/Sidebar.tsx";
-// import {useSelector} from "react-redux";
-// import type {RootState} from "@/redux/stores/store.ts";
 
 export default function DashboardLayout ({ children }:{ children: ReactNode }) {
-
-    // const { user, token } = useSelector( (state: RootState) => state.auth )
-
     return (
         <main className={"w-full h-[88vh]"}>
             <Navbar />
             <div className={"w-full h-full flex"}>
                 <Sidebar />
-                <div className={"w-full h-full"}>{ children }</div>
+                <div className={"w-full h-full overflow-auto"}>{ children }</div>
             </div>
         </main>
     )
