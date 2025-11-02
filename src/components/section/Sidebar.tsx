c5b560fd3477761836984812f7e93716f879980e
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-02 22:00:20 +0600

    admin and subAdmin const value added

--- src/components/section/Sidebar.tsx
+++ src/components/section/Sidebar.tsx
@@ -1,14 +1,4 @@
-import {
-    Banknote,
-    Calendar1, CalendarDays, CalendarMinus,
-    ChartNetwork,
-    ClipboardPlus, CreditCard,
-    LogOut,
-    LucideLayoutDashboard, MessageCircleQuestionMark, ReceiptText, Siren,
-    TrainFrontTunnel,
-    UserRound,
-    UsersRound
-} from "lucide-react";
+import {LogOut} from "lucide-react";
 import Cookies from "js-cookie";
 import {TOKEN_NAME} from "@/enum/token.enum.ts";
 import {toast} from "sonner";
@@ -18,130 +8,29 @@ import {USER_ROLE_ENUM} from "@/enum/role.enum.ts";
 import {useSearchParams} from "react-router-dom";
 import {useEffect} from "react";
 import {setTabOnState} from "@/redux/reducers/tabSlice.ts";
+import {admin, subAdmin} from "@/enum/tab.enum.ts";
 
 export default function Sidebar () {
 
     const { user } = useSelector( (state: RootState ) => state.auth );
-
     const dispatch = useDispatch();
-
     const [searchParams, setSearchParams] = useSearchParams();
-
-    const admin = [
-        {
-            icon: LucideLayoutDashboard,
-            title: "Overview",
-        },
-        {
-            icon: TrainFrontTunnel,
-            title: "Sub Admin Management",
-        },
-        {
-            icon: UserRound,
-            title: "Employee Management",
-        },
-        {
-            icon: ChartNetwork,
-            title: "Profit",
-        },
-        {
-            icon: ClipboardPlus,
-            title: "HRMC Audit Report",
-        },
-        {
-            icon: UsersRound,
-            title: "Audit Log",
-        },
-        {
-            icon: CreditCard,
-            title: "Subscription Management",
-        },
-        {
-            icon: ReceiptText,
-            title: "Terms & Condition",
-        },
-        {
-            icon: Siren,
-            title: "Privacy Policy",
-        },
-        {
-            icon: UserRound,
-            title: "My Profile",
-        },
-        {
-            icon: MessageCircleQuestionMark,
-            title: "Help & Support",
-        },
-    ]
-
-    const subAdmin = [
-        {
-            icon: LucideLayoutDashboard,
-            title: "Overview",
-        },
-        {
-            icon: UsersRound,
-            title: "User Management",
-        },
-        {
-            icon: Calendar1,
-            title: "Attendance Management",
-        },
-        {
-            icon: CalendarMinus,
-            title: "Leave Management",
-        },
-        {
-            icon: CalendarDays,
-            title: "Task Management",
-        },
-        {
-            icon: Banknote,
-            title: "Pay Sli",
-        },
-        {
-            icon: CreditCard,
-            title: "Subscription Management",
-        },
-        {
-            icon: ReceiptText,
-            title: "Terms & Condition",
-        },
-        {
-            icon: Siren,
-            title: "Privacy Policy",
-        },
-        {
-            icon: UserRound,
-            title: "My Profile",
-        },
-        {
-            icon: MessageCircleQuestionMark,
-            title: "Help & Support",
-        },
-    ]
+    const activeTab = searchParams.get("tab");
 
     const logOut = () => {
-
         Cookies.remove(TOKEN_NAME.USER_INFO);
         Cookies.remove(TOKEN_NAME.HRM_FIRM);
-
-        toast.info("Your was successfully logged out.");
-
         window.location.reload();
     }
-
     const setTab = (tab: string) => {
         setSearchParams({tab});
         dispatch(setTabOnState({tab}));
         toast.success(`${tab} active successfully!`, {position: "top-right"});
     }
 
-    const activeTab = searchParams.get("tab");
-
     useEffect(() => {
         setTab(admin[0].title)
-    },[])
+    },[]);
 
     return (
         <div className={"w-[400px] h-full flex justify-center items-center relative"}>
