a4fa8bd8cd1010a7397a3220e54a81814c55a623
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-04 10:06:19 +0600

    Updated

---
+++ src/components/tabs/HelpAndSupport.tsx
@@ -0,0 +1,237 @@
+import {Button} from "@/components/ui/button.tsx";
+import TableRow from "@/components/ui/TableRow.tsx";
+import Table from "@/components/ui/Table.tsx";
+import TableRowCol from "@/components/ui/TableRowCol.tsx";
+import ImageWithSkeleton from "@/components/ui/ImageWIthSkeleton.tsx";
+import ImageStatic from "/auth/google.png";
+import {copyToClipboard} from "@/lib/copyClipboard.ts";
+
+export default function HelpAndSupport() {
+
+    return (
+        <div className={"w-full h-full p-6"}>
+            {/* Header section */}
+            <div className={"flex justify-between items-center my-3"}>
+                <h1 className={"text-3xl font-semibold"}>Help & Support</h1>
+                {/*<div className={"gap-2 flex z-20"}>*/}
+                {/*    <Button className={"bg-[#125BAC] text-white rounded-full cursor-pointer"}>Add Sub Admin</Button>*/}
+                {/*    <DropdownMenu>*/}
+                {/*        <DropdownMenuTrigger asChild>*/}
+                {/*            <Button variant="outline" className={"cursor-pointer"}>Subscription</Button>*/}
+                {/*        </DropdownMenuTrigger>*/}
+                {/*        <DropdownMenuContent className="w-56 border rounded-xl bg-white p-4 mr-8 mt-4" align="start">*/}
+                {/*            <DropdownMenuLabel className={"font-semibold border-b pb-2"}>Subscription status</DropdownMenuLabel>*/}
+                {/*            <DropdownMenuGroup className={""}>*/}
+                {/*                <DropdownMenuItem onClick={() => setStatus("active")} className={"p-2 bg-green-100 rounded-full text-center cursor-pointer my-3 hover:shadow-sm font-semibold"}>Active</DropdownMenuItem>*/}
+                {/*                <DropdownMenuItem onClick={() => setStatus("in-active")} className={"p-2 bg-red-100 rounded-full text-center cursor-pointer hover:shadow-sm font-semibold"}>In-Active</DropdownMenuItem>*/}
+                {/*            </DropdownMenuGroup>*/}
+                {/*        </DropdownMenuContent>*/}
+                {/*    </DropdownMenu>*/}
+                {/*</div>*/}
+            </div>
+            {/* Table */}
+            <Table totalPages={10}>
+                {/* Table Heading*/}
+                <TableRow
+                    className={"bg-[#C7E2FF] h-[60px] border-0 font-semibold"}
+                >
+                    <TableRowCol>
+                        <h3>SL</h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3>Image</h3>
+                    </TableRowCol>
+                    <TableRowCol >
+                        <h3>Name</h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3>ID</h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3>Category</h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3>Contact Number</h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3>Issue Title</h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3>Issue Date</h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3>Status</h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3>Action</h3>
+                    </TableRowCol>
+                </TableRow>
+                {/* Table body or content */}
+                <TableRow
+                    className={"h-[60px] border rounded-none"}
+                >
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("1")}
+                        >
+                            1
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <div className={"w-[30px] h-[30px] rounded-full overflow-hidden"}>
+                            <ImageWithSkeleton src={ImageStatic} />
+                        </div>
+                    </TableRowCol>
+                    <TableRowCol >
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("MD Sohidul Islam Ananto")}
+                        >
+                            MD Sohidul Islam Ananto
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("234234k234239ksdasdfasdfsadh")}
+                        >
+                            234234k234239ksdasdfasdfsadh
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("System")}
+                        >
+                            System
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("01383938337")}
+                        >
+                            018009393842
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("2113")}
+                        >
+                            Problem on Notification
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("stander")}
+                        >
+                            { new Date (Date.now()).toDateString() }
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <div className={"w-full h-full flex justify-center items-center"}>
+                            <Button className={"border border-[#008F37] bg-[#E6F4EB] text-black font-normal"}>Solved</Button>
+                        </div>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <div className={"w-full h-full flex justify-center items-center"}>
+                            <Button className={"border border-[#008F37] bg-[#E6F4EB] text-black font-normal"}>View Details</Button>
+                        </div>
+                    </TableRowCol>
+                </TableRow>
+                <TableRow
+                    className={"h-[60px] border rounded-none"}
+                >
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("1")}
+                        >
+                            2
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <div className={"w-[30px] h-[30px] rounded-full overflow-hidden"}>
+                            <ImageWithSkeleton src={ImageStatic} />
+                        </div>
+                    </TableRowCol>
+                    <TableRowCol >
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("skKKd")}
+                        >
+                            dkshss
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("234234234")}
+                        >
+                            asdfas
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("System")}
+                        >
+                            System
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("01383938337")}
+                        >
+                            018009393842
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("2113")}
+                        >
+                            Problem on Notification
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <h3
+                            className="text-sm max-w-[80px] truncate whitespace-nowrap overflow-hidden cursor-pointer"
+                            title={"Click to copy on clipboard"}
+                            onClick={() => copyToClipboard("stander")}
+                        >
+                            { new Date (Date.now()).toDateString() }
+                        </h3>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <div className={"w-full h-full flex justify-center items-center"}>
+                            <Button className={"border border-orange-400 bg-orange-100 text-black font-normal"}>InProgress</Button>
+                        </div>
+                    </TableRowCol>
+                    <TableRowCol>
+                        <div className={"w-full h-full flex justify-center items-center"}>
+                            <Button className={"border border-[#008F37] bg-[#E6F4EB] text-black font-normal"}>View Details</Button>
+                        </div>
+                    </TableRowCol>
+                </TableRow>
+            </Table>
+        </div>
+    )
+}
